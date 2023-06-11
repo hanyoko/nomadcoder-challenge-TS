6436bcedd167b482290b625c0a4e6d1e70c4b4c8
yohan <koyohanyoko@gmail.com>
2023-06-11 21:05:39 +0900

    노마드코더 챌린지 타입스크립트 14일차

---
+++ 14일차/lodash.d.ts
@@ -0,0 +1,11 @@
+declare module "lodash" {
+    function head(array: any): typeof array[0]
+    function hasIn(key: Object): boolean
+    function isBoolean(value: boolean): boolean
+    function toString(value: string): string
+    function split(string: string | number): string
+    function hasPath(object: Object): boolean
+    function filter(Array: string): string
+    function every(Array: boolean): boolean
+    function map(Array: string): string
+    }
